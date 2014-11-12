80ffa7f309dbc2c41ab8c68c0a0d3f400f6c1f37
Krasimir Kirilov <kkirilov91@gmail.com>
2014-11-12 23:38:04 +0200

    Remove bad code

--- Parser.js
+++ Parser.js
@@ -4,12 +4,12 @@
 	Static functions for parsing
 */
 
+//TODO: Not implemented yet
 exports.parseIniToJson = function(file) {
 	var length = file.length,
 		newFile = '',
 		start = -1,
 		wordStarted = false;
-		curlBrakeds = 0,
 		word = '',
 		isAttribute = false,
 		attribute = '';
@@ -20,34 +20,12 @@ exports.parseIniToJson = function(file) {
 		}
 
 		if (file[i] === ']' && !wordStarted) {
-			curlBrakeds--;
 			isAttribute = false;
 		}
 
-		if (file[i] === '"') {
+		if (file[i] === '') {
 			if (wordStarted) {
-				//TODO FIX this too slow, no need
-				word = file.substring(start+1, i);
-				wordStarted = false;
-
-				if (curlBrakeds === 1) {
-					newFile += '\n\n';
-					newFile += "[" + word + "]"
-				}
-
-				if (curlBrakeds === 2) {
-					if(isAttribute) {
-					//TODO FIX this too slow, no need
-						attribute =  file.substring(start+1, i);
-						isAttribute = false;
-					} else {
-						newFile += '\n' + attribute + '=' + word;
-						isAttribute = true;
-					}
-				}
 			} else {
-				wordStarted = true;
-				start = i;
 			}
 		}
 	};
